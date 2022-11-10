309323f2a51c29d01d47fc4defc223eccdb93591
Hyungwon Lee <hueleev@gmail.com>
2022-11-10 17:05:20 +0900

    2.2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
-ctx.lineWidth = 1;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+
 function onMove(event) {
   const {offsetX, offsetY} = event;
   if (isPainting) {
@@ -20,8 +21,16 @@ function startPainting() {
 }
 function stopPainting() {
   isPainting = false;
+  ctx.beginPath();
 }
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+  
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
