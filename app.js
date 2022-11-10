1cca9a283ab27608edde23220905127e94a9680e
Hyungwon Lee <hueleev@gmail.com>
2022-11-10 11:18:35 +0900

    1.4

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 100);
-ctx.rect(200, 50, 100, 100);
-ctx.rect(300, 150, 100, 100);
+// 좌표 이동 
+ctx.moveTo(50, 50); 
+// 현재 좌표~ lineTo 좌표까지 line
+ctx.lineTo(150, 50); 
+ctx.lineTo(150, 150);  
+ctx.lineTo(50, 150);  
+ctx.lineTo(50, 50);  
+// 그리기
 ctx.fill();
-
-ctx.beginPath(); //새로운 path를 그리지 않으면 아래 스타일이 적용됨 
-ctx.rect(400, 250, 100, 100);
-ctx.rect(500, 350, 100, 100);
-ctx.fillStyle = "red";
-setTimeout(()=> {ctx.fill();}, 1000);
+// ctx.stroke(); 
